d8ec4ba488cee6bdf523ce6b7132daf720799d94
Anindya Sundar Paul <aspaul.93@gmail.com>
2016-05-23 01:20:27 +0600

    add signup signin signout functionality

--- logic/views.py
+++ logic/views.py
@@ -3,11 +3,17 @@ import urllib
 import urllib2
 
 from django.contrib.auth.hashers import check_password, make_password
-from django.http.response import HttpResponse
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+
+from logic.parsers import parse_user
 
 
 # Create your views here.
+dbhost = "http://127.0.0.1:8081/"
+
 def index(request):
     return render(request, "index.html")
 
@@ -16,107 +22,77 @@ def enter(request):
 
 def signout(request):
     request.session.flush()
-    return render(request, "index.html")
+    return HttpResponseRedirect(reverse(index))
 
 def signup(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        email = request.POST['email']
-        
-        url = "http://127.0.0.1:8081/user_exists/"
-        data = {'username':username}
-        data = urllib.urlencode(data)
-        req = urllib2.Request(url, data)
-        response = urllib2.urlopen(req)
-        response = response.read()
-        if(response == "1"):
-            data = {'status':'1'}  # error
-            return render(request, "enter.html", data)
+    
+    username = request.POST['username']
+    
+    data = { 'username': username }
+    response = make_query(dbhost + "get_user/", data)
+    username, _, _, _, _, _ = parse_user(response)
+    
+    if(username is not None):
+        return HttpResponseRedirect(reverse(index))
 
-        url = "http://127.0.0.1:8081/store_user/"
-        code = random.randint(10000, 100000000)
-        code = str(code)
-        password = make_password(password)
-        data = {'username':username, 'password':password, 'email':email, 'code':code}
-        data = urllib.urlencode(data)
-        req = urllib2.Request(url, data)
-        response = urllib2.urlopen(req)
-        response = response.read()
+    username = request.POST['username']
+    email = request.POST['email']
+    password = make_password(request.POST['password'])
+    code = genCode()
+    verified = "0"
+    data = {'username': username, 'email':email, 'password':password, 'code':code, 'verified': verified}
+    response = make_query(dbhost + "set_user/", data)
+    
+    msg = 'Your verification code is: ' + code
+    send_mail('Verification code', msg, 'getfitwebproject@gmail.com', [email], fail_silently=True)
 
-        data = {'username':username}
-        return render(request, "verify.html", data)
-        
-    return HttpResponse("Error!!!")
+    data = {'username': username }
+    return render(request, "verify.html", data)
 
 def verify(request):
     
-    if request.method == 'POST':
-
-        username = request.POST['username']
-        code = request.POST['token']
-
-        url = "http://127.0.0.1:8081/get_code/"
-        data = {'username':username }
-        data = urllib.urlencode(data)
-        req = urllib2.Request(url, data)
-        response = urllib2.urlopen(req)
-        response = response.read()
-        
-        if(response == code):
-            request.session['username'] = username
-            url = "http://127.0.0.1:8081/make_verify/"
-            data = {'username':username }
-            data = urllib.urlencode(data)
-            req = urllib2.Request(url, data)
-            response = urllib2.urlopen(req)
-            response = response.read()
-            return render(request, "index.html")
-        
-        else:
-            data = {'username': username, 'status':'1'}  # error
-            return render(request, "verify.html", data)
-
+    username = request.POST['username']
+    
+    data = { 'username': username }
+    response = make_query(dbhost + "get_user/", data)
+    username, email, password, _, code, _ = parse_user(response)
+    
+    if(code == request.POST['code']):
+        request.session['username'] = username
+        data = {'username': username, 'email': email, 'password':password, 'code':code, 'verified':"1"}
+        response = make_query(dbhost + "set_user/", data)
+        return HttpResponseRedirect(reverse(index))
+    
+    else:
+        data = {'username': username}  # error
+        return render(request, "verify.html", data)
 
 def signin(request):
+
+    username = request.POST['username']
+
+    data = { 'username': username }
+    response = make_query(dbhost + "get_user/", data)
+    username, _, password, _, _, verified = parse_user(response)
     
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+    if(username is None):
+        return HttpResponseRedirect(reverse(enter))
 
-        url = "http://127.0.0.1:8081/user_exists/"
+    if(verified == "0"):
         data = {'username':username}
-        data = urllib.urlencode(data)
-        req = urllib2.Request(url, data)
-        response = urllib2.urlopen(req)
-        response = response.read()
-        if(response == "0"):
-            data = {'status':'1'}  # error
-            return render(request, "enter.html", data)
+        return render(request, "verify.html", data)
 
-        url = "http://127.0.0.1:8081/is_verified/"
-        data = {'username':username}
-        data = urllib.urlencode(data)
-        req = urllib2.Request(url, data)
-        response = urllib2.urlopen(req)
-        response = response.read()
+    if check_password(request.POST['password'], password):
+        request.session['username'] = username
 
-        if(response == "0"):  # not verified
-            data = {'username':username}
-            return render(request, "verify.html", data)
+    return HttpResponseRedirect(reverse(index))
 
+def make_query(url, data):
+    data = urllib.urlencode(data)
+    req = urllib2.Request(url, data)
+    response = urllib2.urlopen(req)
+    response = response.read()
+    return response
 
-                                            # verified
-        url = "http://127.0.0.1:8081/get_password/"
-        data = {'username':username }
-        data = urllib.urlencode(data)
-        req = urllib2.Request(url, data)
-        response = urllib2.urlopen(req)
-        response = response.read()
-        if check_password(password, response):
-            request.session['username'] = username
-        else:
-            data = {'status':'1'}  # error
-            return render(request, "index.html", data)
-
-        return render(request, "index.html")
+def genCode():
+    return str(random.randint(10000, 100000000))
